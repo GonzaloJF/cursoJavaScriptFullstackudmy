6c0012c348c5ef90ef24e454e7a25cad66f7f384
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2023-03-11 15:49:37 -0300

    asd

---
+++ JavaScript/primer.js
@@ -0,0 +1,3 @@
+var variable = 'este es un tipo de variable generico'
+const variable2 = 'este es un tipo de variable constante'
+let variable3 = 'este es un tipo de variable donde el nombre estara bloqueado'
