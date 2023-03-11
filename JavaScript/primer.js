570d9331986f5b2cbd3b376e94fa41fb3e04344e
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2023-03-11 16:50:08 -0300

    comienzo de aprender js

--- JavaScript/primer.js
+++ JavaScript/primer.js
@@ -1,3 +1,52 @@
+/**********Primeros pasos en JavaScript comienzo de Variables**********/
 var variable = 'este es un tipo de variable generico'
 const variable2 = 'este es un tipo de variable constante'
 let variable3 = 'este es un tipo de variable donde el nombre estara bloqueado'
+
+let string1 = 'estoy haciendo un curso de javaScript'
+let string2 = 'Estoy aprendiendo bien javaScript'
+let resultado= `${string1} ${string2}`
+
+//typeof variable, sirve para revisar que tipo de variable es.
+
+/*********comienzo de estudios de objetos*******************/
+let string3 = 'nuevo string'
+typeof string3
+let numero1 = 2
+typeof numero1
+let objeto = {
+    propiedad1: 'Texto1',
+    propiedad2: 2,
+    propiedad3: {
+
+    }
+}
+typeof objeto
+objeto.propiedad1
+typeof objeto.propiedad1
+objeto.propiedad2
+typeof objeto.propiedad2
+objeto.propiedad3
+typeof objeto.propiedad3
+null
+/***********Final del estudio de objetos********************/
+/**Estudio de más primitivas**/
+//variables undefined, booleanos.
+undefined
+typeof undefined
+let var1 = undefined
+let var2 = true
+let var3 = false
+let var4 
+typeof var1
+typeof var2
+typeof var3
+typeof var4
+//symbol
+typeof Symbol()
+/**Final de las primitivas**/
+
+/**Expresiones= es todo lo que esta al lado derecho de la variable definida**/
+let algo ='primera expresion'
+var algo2= 2
+const algo3= true
