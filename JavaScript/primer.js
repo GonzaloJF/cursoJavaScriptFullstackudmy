08371826a1f72797dd8f0798103a1913f56d3d7d
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2023-03-14 18:57:39 -0300

    parte de funciones, iteraciones y metodos

--- JavaScript/primer.js
+++ JavaScript/primer.js
@@ -98,3 +98,47 @@ var z='soy la primera variable'
     u
 }
 z
+
+/** Funciones **/
+
+function sumar(num1, num2){
+    console.log(arguments);
+    const resultado = num1 + num2;
+    return resultado;
+}
+
+const r = sumar(1,2)
+const t = sumar(-6,7)
+const z = sumar(r,t)
+
+r
+t
+z
+
+sumar(3,6)
+
+/**Metodos e iteraciones**/
+
+const persona1 = {
+    nombre: 'Maria',
+    apellido: 'Martinez',
+    id: 5,
+    nombreCompleto: function(){
+        console.log(arguments);
+        return `${this.nombre} ${this.apellido}`
+    }
+}
+
+persona1.nombreCompleto()
+
+/**arrays**/
+
+const miArray=[ 1, 2, 3, 'hola', 28, {a:1} ]
+
+miArray
+
+typeof miArray
+
+var iterador = miArray [Symbol.iterator]();
+
+iterador.next().value
