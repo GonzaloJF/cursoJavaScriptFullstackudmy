9be7541317d598473d9cb22aa0c854bb75827a4e
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2023-03-22 01:33:32 -0300

    a

--- JavaScript/destructuracion.js
+++ JavaScript/destructuracion.js
@@ -27,3 +27,33 @@ miArray
 let [var1,var2,var3,var4,var5] = miArray
 
 var1
+
+//En caso de querer omitir los primeros datos dentro de un array, se puede hacer de la siguiente manera
+
+let[var7,var6, ...resto] = miArray
+
+resto
+
+//Para poder cambiar datos dentro de los objetos por medio de la destructuración es de la siguiente manera:
+
+miObjeto
+
+let miObjeto2 = miObjeto
+
+miObjeto2
+
+miObjeto2.c="otra cosa"
+
+miObjeto
+
+//copia superficial
+
+miObjeto3 = {...miObjeto}
+
+miObjeto3
+
+miObjeto3.c = "string totalmente nuevo"
+
+miObjeto3
+
+miObjeto
