f46582b515908ff1cd3d1b183bc8e4fae3ea15c5
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2023-04-06 18:37:42 -0400

    emplo de ajax listo

--- JavaScript/ajax/ejemplo_1_ajax.js
+++ JavaScript/ajax/ejemplo_1_ajax.js
@@ -1,17 +1,38 @@
 //ajax = asynchronus JavaScript And Xml
 const listaUsuarios = document.getElementById("Lista_usuarios");
+const boton = document.getElementById("boton_enviar");
 
 function reqListener(){
     const usuarios = JSON.parse(this.responseText);
-    const usuariosRender = usuarios.map((individuo)=> `<li>${individuo.nombre}</li>`).join(" ");
+    console.log(usuarios);
+    const usuariosRender = usuarios.map((individuo)=> `<li>${individuo.nombre}</li>`).join("");
     console.log(usuariosRender);
     listaUsuarios.innerHTML = usuariosRender;
 }
 
 var peticion = new XMLHttpRequest();
 peticion.addEventListener("load",reqListener);
-peticion.open("GET", "http://bootcamp-dia-3.camilomontoyau.now.sh/usuarios");
-peticion.send();
+
+function enviarDatos(){
+    peticion.open(
+        "POST", 
+        "http://bootcamp-dia-3.camilomontoyau.now.sh/usuarios", 
+        true
+    );
+    peticion.setRequestHeader(
+        "Content-type", 
+        "application/x-www-form-urlencoded"
+    );
+    peticion.send("nombre=Alejandro");
+    setTimeout(refrescar,3000); 
+}
+
+function refrescar(){
+    peticion.open("GET", "http://bootcamp-dia-3.camilomontoyau.now.sh/usuarios");
+    peticion.send();    
+}
+
+boton.onclick = enviarDatos
 
 
 //document tiene una funcion que lo que nos sirve es para poder obtener elementos segun el id
